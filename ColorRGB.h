74b9ed261f52f189935470dc90cf42ed2ac5f800
Franz <franzbeltran79@gmail.com>
2019-03-19 15:24:53 +0100

    Crear un RayTracer basico

--- ColorRGB.h
+++ ColorRGB.h
@@ -1,5 +1,8 @@
+#ifndef COLORRGB_H
+#define COLORRGB_H
 struct ColorRGB {
     double r;
     double g;
     double b;
 };
+#endif
--- main.cpp
+++ main.cpp
@@ -1,21 +1,125 @@
+#include <vector>
+#include <iostream>
+
+#include "Vector3D.h"
+#include "Esfera.h"
+#include "ViewPlane.h"
+
 #include "Utilitarios.h"
+
+
+
+// #include "ObjetoGeometrico.h"
+
 using namespace std;
+
+ColorRGB obtenerColorPixel(const Rayo& r, vector<ObjetoGeometrico*> objetos){
+    
+    ColorRGB color;
+
+    color.r = 0.0;
+    color.g = 0.0;
+    color.b = 0.0;
+
+    double t;
+    double tmin = 2000000;    
+    Vector3D n;
+    Punto3D q;
+
+    Vector3D h;
+    for(int i = 0; i < objetos.size(); i++) {
+        if( objetos[i]->hayImpacto(r, t, n, q) && t < tmin){
+            color.r = 1.0;
+            color.g = 0.0;
+            color.b = 0.0;
+
+            tmin = t;
+        }
+    }
+    return color;
+}
+
+
 int main() {
-    int dpi = 72;
-    int width = 640;
-    int height = 480;
+    // int dpi = 72;
+    // int width = 640;
+    // int height = 480;
+    // int n = width * height;
+    // ColorRGB* pixeles = new ColorRGB[n];
+    // for(int x= 0; x < width; x++)
+    // {
+		// for( int y = 0; y < height; y++)
+		// {
+		// 	pixeles[y*width+x].r = 0;
+		// 	pixeles[y*width+x].g = 0;
+		// 	pixeles[y*width+x].b = 1.0;
+		// }
+
+    // }
+    // savebmp("img.bmp", width, height, dpi, pixeles);
+    // Vector3D v(2.0,-1.2,3.0);
+    // v.mostrar_vector();
+
+
+    // ESCENA
+
+    // ESCENA--------------------------------------------------------------------------------------------
+    Punto3D centro1(0.0, 0.0, -500.0);
+    double radio1 = 120;
+    Esfera esfera1(centro1, radio1);   
+    esfera1.establecerColor(0.30, 0.40, 0.10);
+
+    Punto3D centro2(220.0, 0.0, 0.0);
+    double radio2 = 100;
+    Esfera esfera2(centro2, radio2);   
+    esfera2.establecerColor(0.23, 1.0, 0.80);
+
+
+    vector<ObjetoGeometrico*> escena;
+    escena.push_back(&esfera1);
+    escena.push_back(&esfera2);
+
+
+    ColorRGB color_pixel;
+
+    // VIEWPLANE
+    int hres = 800;
+    int vres = 600;
+    double s = 1.0;
+    ViewPlane vp(hres, vres, s);
+
+
+
+    // UTILITARIO PARA GUARDAR IMAGEN -------------------------------------------------------------------
+     int dpi = 72;
+    int width = vp.hres;
+    int height = vp.vres;
     int n = width * height;
     ColorRGB* pixeles = new ColorRGB[n];
-    for(int x= 0; x < width; x++)
+
+
+    
+
+    // ALGORITMO
+    for(int fil = 0; fil < vp.vres; fil++)
     {
-		for( int y = 0; y < height; y++)
-		{
-			pixeles[y*width+x].r = 0;
-			pixeles[y*width+x].g = 0;
-			pixeles[y*width+x].b = 1.0;
-		}
-        
-    }
+        for ( int col = 0; col < vp.hres; col++) 
+        {
+             // Disparar un rayo
+            Vector3D direccion(0.0, 0.0, -1.0);
+            double x_o = vp.s * ( col - vp.hres/2 + 0.5 );
+            double y_o = vp.s * ( fil - vp.vres/2 + 0.5 );
+            double z_o = 100;
+            Punto3D origen(x_o, y_o, z_o);
+            Rayo rayo(origen, direccion);
+
+            color_pixel = obtenerColorPixel(rayo, escena);
+
+            pixeles[fil*width+col].r = color_pixel.r;
+            pixeles[fil*width+col].g = color_pixel.g;
+            pixeles[fil*width+col].b = color_pixel.b;
+        }
+    }    
     savebmp("img.bmp", width, height, dpi, pixeles);
     return 0;
 }
